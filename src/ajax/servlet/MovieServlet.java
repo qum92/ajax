c7fb1bcd255759e41a87c33b1b7b67bd50380227
Qum92 <oscar9992@naver.com>
2019-04-01 17:48:22 +0900

    updated controller

--- src/ajax/servlet/MovieServlet.java
+++ src/ajax/servlet/MovieServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import service.MovieService;
 import service.impl.MovieServiceImpl;
@@ -46,14 +47,23 @@ public class MovieServlet extends HttpServlet {
 		}else {
 			String cmd = uri.substring(idx+1);
 			if("insert".equals(cmd)) {
+				HttpSession hs = request.getSession();
+				if(hs.getAttribute("user")==null) {
+					request.setAttribute("msg", "로그인하세요");
+					request.setAttribute("url", "/");
+					RequestDispatcher rd = request.getRequestDispatcher("/views/msg/result");
+					rd.forward(request, response);
+					return;
+				}
 				Map<String,String> movie = new HashMap<>();
 				movie.put("mi_name", request.getParameter("mi_name"));
 				movie.put("mi_year", request.getParameter("mi_year"));
 				movie.put("mi_national", request.getParameter("mi_national"));
 				movie.put("mi_vendor", request.getParameter("mi_vendor"));
 				movie.put("mi_director", request.getParameter("mi_director"));
+				request.setAttribute("msg", "영화등록실패!");
 				if(ms.insertMovie(movie)==1) {
-					request.setAttribute("msg", "등록완료");
+					request.setAttribute("msg", "영화등록완료");
 					request.setAttribute("url", "/movie/list");
 				}
 				RequestDispatcher rd = request.getRequestDispatcher("/views/msg/result");
